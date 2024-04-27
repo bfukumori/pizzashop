acb633007deed1f9ef24ca14ac07872218f05eaf
Bruno Fukumori <brunofukumori@gmail.com>
2024-04-27 02:35:42 -0300

    feat: add not found and error page

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,11 +7,12 @@ import { Dashboard } from './pages/app/dashboard'
 import { Orders } from './pages/app/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
+import { ErrorPage } from './pages/error'
 
 export const router = createBrowserRouter([
   {
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
@@ -23,5 +24,9 @@ export const router = createBrowserRouter([
       { path: '/sign-in', element: <SignIn /> },
       { path: '/sign-up', element: <SignUp /> },
     ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ])
